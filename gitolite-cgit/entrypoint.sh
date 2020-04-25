c060244680f6e975a99a9b614598ad88e2eadefb
HaiBac Ngo <ngohaibac@gmail.com>
2020-04-25 12:40:09 +0700

    add correct config .env

--- gitolite-cgit/entrypoint.sh
+++ gitolite-cgit/entrypoint.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # Running once container starts
 if [ ! -f "/var/lib/git/.init" ]; then
-  
 
+  # Setup gitolite admin
+  echo "$SSH_KEY" > "/tmp/$SSH_KEY_NAME.pub"
+  su git -c "gitolite setup -pk /tmp/$SSH_KEY_NAME.pub"
+  
+  touch /var/lib/git/.init
+
+  exec "$@"
 fi
 
 # exec other cmds
