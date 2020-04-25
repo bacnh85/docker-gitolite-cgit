cd2d3410d4e0c4f6609ccd0d7102b7d806affc01
HaiBac Ngo <ngohaibac@gmail.com>
2020-04-25 20:53:52 +0700

    add sshd
    Signed-off-by: HaiBac Ngo <ngohaibac@gmail.com>

--- gitolite-cgit/entrypoint.sh
+++ gitolite-cgit/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-# Setup gitolite admin
-if [ ! -f "/var/lib/git/.ssh/authorized_keys" ]; then
+# Create ssh host key if not present
+if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
+  ssh-keygen -A
+fi
 
+# Configure gitolite
+if [ ! -f "/var/lib/git/.ssh/authorized_keys" ]; then  
   echo "$SSH_KEY" > "/tmp/$SSH_KEY_NAME.pub"
-  su git -c "gitolite setup -pk /tmp/$SSH_KEY_NAME.pub" 
-
-  exec "$@"
+  su git -c "gitolite setup -pk /tmp/$SSH_KEY_NAME.pub"  
 fi
 
 # exec other cmds
-exec "$@"
+exec /usr/sbin/sshd -D -e $@
